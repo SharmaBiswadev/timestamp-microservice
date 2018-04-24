a77748e0b9a36d6359761111b9d6e600c33014d7
Biswadev <sharmabiswadev2@gmail.com>
2018-04-24 23:45:55 +0530

    added microservice

--- app.js
+++ app.js
@@ -7,24 +7,39 @@ var bodyparser=require('body-parser');
 app.route('/:date')
     .get(function(req, res) {
 // res.sendFile(process.cwd() + '/views/index.html');
-     let unix,naturaldate;
-     let timestamp=req.params.date;
-     console.log(timestamp)
-     if(typeof parseInt(timestamp)==="number"){
-       let parsenaturaldate=(new Date(1512086400000)).toDateString().split(" ").slice(1);
-       let year=parsenaturaldate.insert(2,',');
-       naturaldate=parsenaturaldate.join("");
+       var unix=null,naturaldate=null;
+     //console.log(parseInt(timestamp),timestamp)
+      let timestamp=req.params.date;
+     if(!isNaN(timestamp)){
+     	let parsetime=new Date(timestamp)
+     	console.log(parsetime);
+     	if(parsetime=='Invalid Date'){
+     		console.log("got invalid date")
+     	 send_response(null,null,res);
+     	}
+       let parsenaturaldate=(new Date(timestamp)).toDateString().split(" ").slice(1);
+       let year=parsenaturaldate.splice(2,0,',');
+       year=parsenaturaldate.splice(1,0,' ')
+       naturaldate=parsenaturaldate.join("")
        unix=parseInt(timestamp)
+       send_response(unix,naturaldate,res);
      }else {
         unix=Date.parse(timestamp);
+        console.log(unix);
+        if(!isNaN(unix))
         naturaldate=timestamp
+        else
+       return send_response(null,null,res);
+    	send_response(unix,naturaldate,res);
      }
-     
+ })
+     function send_response(unix,naturaldate,res){ 
+         console.log(unix,naturaldate)
           res.json({
       "unix":unix,
       "natural":naturaldate
      })
-     })
+      }
 
     app.listen(8083,()=>{
     	console.log("server is listening")
